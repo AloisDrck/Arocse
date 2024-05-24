557e16da4952bb8b5890ebf258f62729f04ddd2b
Aloïs Drucké <al.drucke@gmail.com>
2024-05-24 16:38:58 +0200

    suppression des images inutiles

--- backend/models/Scoreboard.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const scorboardSchema = mongoose.Schema({
-    playerName: { type: String, required: true },
-    score: { type: Number, required: true },
-    date: { type: Date, default: Date.now },
-});
-
-module.exports = mongoose.model('Scoreboard', scorboardSchema);
